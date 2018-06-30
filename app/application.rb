56375cc03ee0b144ed0daedaea51aeccb5bfbe83
Eric DeSimone <eric.desimone@cortland.edu>
2018-06-30 11:53:26 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -18,6 +18,17 @@ class Application
       resp.write "Path Not Found"
     end
 
+    if req.path.match(/add/)
+      req.path.match(/search/)
+      search_term = req.params["q"]
+      if @@items.include?(search_term)
+        @@cart << search_term && resp.write "added #{search_term}"
+      else 
+        resp.write "We don't have that item"
+    else
+      resp.write "Path Not Found"
+    end
+
     if req.path.match(/cart/)
       if @@cart.count > 0
         @@cart.each do |item|
