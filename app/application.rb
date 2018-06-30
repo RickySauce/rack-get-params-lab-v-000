f83a135f7d37fdd2cafc0799f917862ebb0dc449
Eric DeSimone <eric.desimone@cortland.edu>
2018-06-30 11:53:30 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -23,7 +23,7 @@ class Application
       search_term = req.params["q"]
       if @@items.include?(search_term)
         @@cart << search_term && resp.write "added #{search_term}"
-      else 
+      else
         resp.write "We don't have that item"
     else
       resp.write "Path Not Found"
