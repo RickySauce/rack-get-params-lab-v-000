f734580a90e59d6470504807954c13718075d947
Eric DeSimone <eric.desimone@cortland.edu>
2018-06-30 11:45:28 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -25,6 +25,7 @@ class Application
         end
       else
         resp.write "Your cart is empty"
+      end
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
